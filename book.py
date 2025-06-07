ceb316ab82e3fce51fe62d9ac4d6c18db7a00c4a
Suman <suman@localhost.localdomain>
2025-06-07 12:25:10 -0500

    Schema ready

--- book.py
+++ book.py
@@ -4,6 +4,12 @@ import random
 
 book_list = []
 
+class UpdateBook(BaseModel):
+    bk_name: Optional[str] = None
+    bk_author: Optional[str] = None
+    bk_description: Optional[str] = None
+    bk_price: Optional[str] = None
+
 class Book(BaseModel):
     bk_id:Optional[int]=None
     bk_name:str
