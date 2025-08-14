2c07e5c4cb4506a372bf46361350c3b59bcee683
SumanAch10 <contact.sumanacharya@gmail.com>
2025-08-13 19:14:09 -0500

    feat(User):add User model with initial migrations

--- app/models/user.py
+++ app/models/user.py
@@ -4,7 +4,6 @@ from app.db.base_class import Base
 from app.core.config import settings
 
 class User(Base):
-    
     __tablename__ = "users"
     user_id:Mapped[int] = mapped_column(Integer,primary_key = True,autoincrement = True)
     user_name:Mapped[str] = mapped_column(String(25),nullable = False,unique = True)
